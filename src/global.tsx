c50428136f237926436fd61f0f4e279d050674a7
unknown <paedeogim@gmail.com>
2024-06-03 05:59:59 +0900

    initial commit

---
+++ src/global.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+    return (
+        <HTML1>
+            <HTML2>
+                <HTML3>
+                    ...
+                </HTML3>
+            </HTML2>
+        </HTML1>
+    );
+}
+
+이런 식에서
+
+const Reducer = ...
+
+해서 뭐 
+export default function App() {
+    r
+}
